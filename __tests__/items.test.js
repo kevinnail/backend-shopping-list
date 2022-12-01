9a9fae343bf51d390bf0417d1b4fed1bbc4c9574
kevinnail <fumalicious@gmail.com>
2022-11-30 17:58:05 -0800

    CI clean up again after connecting to front end

--- __tests__/items.test.js
+++ __tests__/items.test.js
@@ -80,8 +80,9 @@ describe('items', () => {
     expect(resp.status).toEqual(401);
   });
 
-  it.only('UPDATE /api/v1/items/:id should update an item', async () => {
+  it('UPDATE /api/v1/items/:id should update an item', async () => {
     // create a user
+
     const [agent, user] = await registerAndLogin();
     const item = await Item.insert({
       description: 'apples',
@@ -92,12 +93,16 @@ describe('items', () => {
       .put(`/api/v1/items/${item.id}`)
       .send({ bought: true });
     expect(resp.status).toBe(200);
+    console.log('resp.body', resp.body);
+
     expect(resp.body).toEqual({ ...item, bought: true });
   });
 
-  it('UPDATE /api/v1/items/:id should 403 for invalid users', async () => {
+  it.only('UPDATE /api/v1/items/:id should 403 for invalid users', async () => {
     // create a user
+
     const [agent] = await registerAndLogin();
+
     // create a second user
     const user2 = await UserService.create(mockUser2);
     const item = await Item.insert({
@@ -108,6 +113,7 @@ describe('items', () => {
     const resp = await agent
       .put(`/api/v1/items/${item.id}`)
       .send({ bought: true });
+
     expect(resp.status).toBe(403);
   });
 
